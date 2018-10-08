635c664980471e79945b7c460669c233b92a5867
rec16005 <rec16005@uvg.edu.gt>
2018-10-08 13:20:33 -0600

    Commit final

--- instagram/resources/actions.py
+++ instagram/resources/actions.py
@@ -10,6 +10,16 @@ def menu():
   print("----------------------------------")
   return selection
 
+def menu_log():
+  print("MENU")
+  print("1. Crear post")
+  print("2. Likear post")
+  print("3. Borrar post")
+  print("4. Salir")
+  selection = input("Ingrese su selección: ")
+  print("----------------------------------")
+  return selection
+
 def create_user():
   name = input("Ingrese el nombre: ")
   last_name = input("Ingrese el apellido: ")
@@ -26,13 +36,44 @@ def show_users():
     print("pk={}: {} {} - {} - {}".format(user.pk, user.name, user.last_name, user.user_name, user.email))
   print("----------------------------------")
 
-def login():
-  user = input("Ingrese username: ")
-  this_email = input("Ingrese email: ")
-  current_id = -1
-  for user in Users.objects.all():
-    if user.user_name == user and user.email == this_email:
-      current_id = user.pk
-      print("Hola {}").format(user.name)
-      break
-  return current_id
+def login(username, email):
+  try:
+    current_user = Users.objects.get(user_name=username, email=email)
+    print("Bienvenido " + current_user.user_name)
+  except Users.DoesNotExist:
+    print("No hay usuarios con ese ID")
+  print("----------------------------------")
+  return current_user
+
+def create_post():
+  title = input("Ingrese el título del post: ")
+  post_body = input("Ingrese el contenido del post: ")
+  new_post = Posts(title = title, post_body = post_body)
+  new_post.save()
+  print("Cantidad despues: ", Posts.objects.all().count())
+  print("----------------------------------")
+
+def show_posts():
+  print("Posts: ")
+  for post in Posts.objects.all():
+    print("Numero={}: {} | {} ({}) -- {}".format(post.pk, post.title, post.post_body, post.number_likes.count(), post.date))
+  print("----------------------------------")
+
+def like_post(current_user, liked_id):
+  try:
+    liked_post = Posts.objects.get(id=liked_id)
+    new_like = Likes(user = current_user, post = liked_post)
+    new_like.save()
+    print("Post likeado: " + liked_post.title)
+  except Posts.DoesNotExist:
+    print("No existe el post")
+  print("----------------------------------")
+
+def delete_post(deleted_id):
+  try:
+    deleted_post = Posts.objects.get(id=deleted_id)
+    deleted_post.delete()
+    print("Cantidad despues: ", Posts.objects.all().count())
+  except Posts.DoesNotExist:
+    print("No existe el post")
+  print("----------------------------------")
--- instagram/resources/management/commands/main.py
+++ instagram/resources/management/commands/main.py
@@ -4,23 +4,46 @@ import resources.actions as action
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
       selec = "-1"
-      current_user_id = "-1"
       while selec != "0":
         selec = action.menu()
 
         if selec == "1":
           action.create_user()
+
         elif selec == "2":
           action.show_users()
+
         elif selec == "3":
-          current_user_id = action.login()
-          if current_user_id == "-1":
-            print("Usuario o email incorrecto")
+          given_username = input("Ingrese el username: ")
+          given_email = input("Ingrese el email: ")
+          current_user = action.login(given_username, given_email)
+          selec2 = "-1"
+
+          while selec2 != "0":
+            selec2 = action.menu_log()
+            if selec2 == "1":
+              action.create_post()
+            elif selec2 == "2":
+              action.show_posts()
+              liked_id = input("Ingrese el numero del post: ")
+              action.like_post(current_user,liked_id)
+            elif selec2 == "3":
+              action.show_posts()
+              deleted_id = input("Ingrese el numero del post: ")
+              action.delete_post(deleted_id)
+            elif selec2 == "4":
+              selec2 = "0"
+            else:
+              print("Opcion incorrecta trate de nuevo")
+              print("----------------------------------")
+
           print("----------------------------------")
+
         elif selec == "4":
           print("Adios!")
           print("----------------------------------")
           selec = "0"
+       
         else:
           print("Opcion incorrecta trate de nuevo")
           print("----------------------------------")
